e0df69807ca2a91f64be4243875c1f21fbe9bfb8
donam26 <91812675+donam26@users.noreply.github.com>
2025-06-15 14:02:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     // Nếu đã đăng nhập, chuyển đến trang quản lý nhà
     if (Auth::check()) {
-        return redirect()->route('houses.index');
+        return redirect()->route('houses.by.username');
     }
     // Nếu chưa đăng nhập, hiển thị trang welcome
     return view('welcome');
